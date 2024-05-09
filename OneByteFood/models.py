88fe2037f92e282750e4c53342eaeef3a519e30a
LoonevaMaharjan <np03cs4a220400@heraldcollege.edu.np>
2024-05-09 08:22:55 +0545

    Database for Menu Page

--- OneByteFood/models.py
+++ OneByteFood/models.py
@@ -13,3 +13,16 @@ class Reservation(models.Model):
     comments = models.TextField()
     payment_option = models.CharField(max_length=20)
     status = models.CharField(max_length=50, default='pending')
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.CharField(max_length=140, default='1')
+
+
+# class Order(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+#     food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
